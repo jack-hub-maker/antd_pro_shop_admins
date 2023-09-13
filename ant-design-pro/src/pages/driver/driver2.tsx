516ce0a1938c68141fedf65c91141131a4bc5c43
xingyingjie <xingyingjie@zswltech.cn.com>
2023-09-13 11:09:42 +0800

    test:123

--- ant-design-pro/src/pages/driver/driver2.tsx
+++ ant-design-pro/src/pages/driver/driver2.tsx
@@ -4,6 +4,7 @@ import Video from './components/Video';
 const Driver2 = () => {
   return (
     <div>
+      123
       <Video videoUrl={'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'} />
     </div>
   );
